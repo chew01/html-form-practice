3d808676b8bff19d8d52adc5205b30dd70541740
chew01 <chewjh19@gmail.com>
2022-01-27 01:06:52 +0800

    added basic clientside validation

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const password = document.querySelector('#password');
+const confirmPassword = document.querySelector('#confirm_password');
 
+const confirmPasswordListener = () => {
+  if (confirmPassword.value !== password.value) {
+    confirmPassword.setCustomValidity('Passwords do not match!');
+  } else confirmPassword.setCustomValidity('');
+};
+
+password.addEventListener('input', confirmPasswordListener);
+confirmPassword.addEventListener('input', confirmPasswordListener);
